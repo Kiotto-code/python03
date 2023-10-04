10cbe45cf7fedb3eca1ad4f414e4ca2a9deeabd8
Chan Yik Wai <86033215+Kiotto-code@users.noreply.github.com>
2023-10-04 21:45:49 +0800

    Update ft_calculator.py

--- ex03/ft_calculator.py
+++ ex03/ft_calculator.py
@@ -10,17 +10,17 @@ class calculator:
 
     def __add__(self, object) -> None:
         """addition method"""
-        self.data = [element + 5 for element in self.data]
+        self.data = [element + object for element in self.data]
         print(self.data)
 
     def __mul__(self, object) -> None:
         """multiplication method"""
-        self.data = [element * 5 for element in self.data]
+        self.data = [element * object for element in self.data]
         print(self.data)
 
     def __sub__(self, object) -> None:
         """substraction method"""
-        self.data = [element - 5 for element in self.data]
+        self.data = [element - object for element in self.data]
         print(self.data)
 
     def __truediv__(self, object) -> None:
@@ -28,5 +28,5 @@ class calculator:
         if object == 0:
             print("ERROR (cannot divide by zero))")
             return
-        self.data = [element / 5 for element in self.data]
+        self.data = [element / object for element in self.data]
         print(self.data)
